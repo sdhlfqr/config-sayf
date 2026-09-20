d9fc7c5f9414987c91065a3e66a763759530574d
Sayf Dhū al-Faqār <sdhlfqr@outlook.com>
2026-09-20 17:20:22 +0000

    feat(pkgs): add Antigravity CLI package

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-{ ... }: {
+{ antigravity, ... }: {
   imports = [
     ./configs/direnv.nix
     ./configs/git.nix
     ./configs/fish.nix
     ./configs/neovim.nix
+  ];
+
+  home.packages = [
+    antigravity.packages.x86_64-linux.google-antigravity-cli
   ];
 
   home.stateVersion = "26.05";
